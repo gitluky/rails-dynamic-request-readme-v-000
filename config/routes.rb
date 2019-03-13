a82043979ecb882da8aece7bdbdc016da220acbd
Jonathan Luk <jluk87@gmail.com>
2019-03-13 23:26:56 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   get 'posts/:id', to: 'posts#show'
+  resources :posts, only: :show
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
